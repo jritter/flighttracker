ff265f34b354b5f56bce12887eef31c86138fa1e
Juerg Ritter <juerg_ritter@bluewin.ch>
2015-07-26 00:34:02 +0200

    fix of Grunt task, airport locations

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -8,7 +8,15 @@
  * Controller of the flightTracker
  */
 angular.module('flightTracker')
-  .controller('MapCtrl', function ($scope) {
+  .controller('MapCtrl', function ($scope, $http) {
     L.mapbox.accessToken = 'pk.eyJ1IjoianJpdHRlciIsImEiOiI1dnN4djFvIn0.CpjDtivgWXdataV9c5Ihxw';
-    var map = L.mapbox.map('map', 'jritter.mphb26eo', { zoomControl: false, legendControl: { position: 'topleft' }})
+    var map = L.mapbox.map('map', 'jritter.mphb26eo', { zoomControl: false, legendControl: { position: 'topleft' }});
+
+    $http({
+      url: 'http://localhost:3000/airports/by-continent/eu.json',
+      method: 'GET',
+      cache: true,
+    }).success(function(geojson){
+      map.featureLayer.setGeoJSON(geojson);
+    });
   });
